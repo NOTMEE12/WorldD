e879c3233af8b2209901c94560bd7618bc76ced8
NOTMEE12 <legominefan@gmail.com>
2023-07-19 15:08:02 +0200

    still trying to get it under pip

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-from setuptools import setup
-setup(name='WorldD',
-      version='0.1',
-      description='',
-      url='https://github.com/NotMEE12/WorldD',
-      author='NotMEE12',
-      author_email='legominefan@gmail.com',
-      license='MIT',
-      packages=['WorldD'],
-      install_requires=['pygame-ce>=2.0.0'])
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='WorldD',
+	version='0.1',
+	scripts=['src/__init__.py', 'src/main.py', 'src/options.toml'] ,
+	author="Aleks Baran",
+	author_email="legominefan@gmail.com",
+	description="Level editor made specifically for pygame-ce",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/NotMEE12/WorldD",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+)
