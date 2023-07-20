70b8c5f17b7f9a84c6da94e9db1dfaefe6c5c4cb
NOTMEE12 <legominefan@gmail.com>
2023-07-20 15:02:18 +0200

    changed some things. maybe now it will work

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
 setuptools.setup(
 	name='WorldD',
 	version='0.1',
-	scripts=['./src/__init__.py', './src/main.py', './src/options.toml'] ,
+	package_dir={"": "src"},
 	author="Aleks Baran",
 	author_email="legominefan@gmail.com",
 	description="Level editor made specifically for pygame-ce",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/NotMEE12/WorldD",
-	packages=setuptools.find_packages(),
+	packages=setuptools.find_packages("src"),
 	classifiers=[
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
