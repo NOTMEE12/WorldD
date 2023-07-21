c1cc5be81d8aa4e087332da3ac8223577349f8f9
NOTMEE12 <legominefan@gmail.com>
2023-07-21 11:44:14 +0200

    maybe now it will build?

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
 setuptools.setup(
 	name='WorldD',
 	version='0.1',
-	package_dir={"": "src/WorldD"},
+	package_dir={"": "WorldD"},
 	author="Aleks Baran",
 	author_email="legominefan@gmail.com",
 	description="Level editor made specifically for pygame-ce",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/NotMEE12/WorldD",
-	packages=setuptools.find_packages("src"),
+	packages=setuptools.find_packages("WorldD"),
 	classifiers=[
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
