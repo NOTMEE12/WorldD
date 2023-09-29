9c15d037bde94c18ce37962d7d628ede84257b48
NOTMEE12 <legominefan@gmail.com>
2023-09-29 18:43:10 +0200

    pep 440 version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name='WorldD',
-	version='p1.0.1',
+	version='1.0.1',
 	author="Aleks Baran",
 	author_email="legominefan@gmail.com",
 	description="Level editor made specifically for pygame games",
